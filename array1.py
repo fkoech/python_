ba02a4abb295053440f83c0478b063231e30ace0
fkoech <fkoech2017@gmail.com>
2020-03-22 12:59:10 +0300

    you have to remove the comments in each line so as to run the pragrams. this practical talks of the arrays, for loop, variables and else conditional statements

---
+++ array1.py
@@ -0,0 +1,145 @@
+###example1 of a single array in python###
+#a=["banana", "apple", "microsoft"]
+#print(a[0])
+#print(a[1])
+#print(a[2])
+#print(a[2])
+  
+
+  
+####example1 of a  for loop in python#######
+#a = [20, 30, 40, 60]
+#for element in a:
+#   print(element)
+#   print(element)
+
+
+
+
+
+
+#for element in b:
+#      print(element)
+#c=[3434, 39595, 394959]
+#ttal = 0
+#fr e in c:
+#total = total + e
+#pint(total)
+
+
+
+##1,2, 3, 4
+#c = list(range(1,5))
+#print(c)
+#for i in range(1, 5):
+#    print(i)
+#
+
+
+
+
+#total = 0
+#for i in range(1,10000):
+# total += i
+# print(total)
+
+
+#total2 = 0
+#for i in  range(1, 100):
+#   total2 +=i
+#
+#print(total2)
+#
+
+
+
+# if condition
+#total2 = 0
+#for i in range(1, 231):
+#    if i % 3 == 0:
+#        total2 += 1
+#        print(total2)
+
+
+
+#a = 39495
+#print(a)
+
+
+
+#b = "my name is Felix Koech Cherop"
+#c = "a student of catholic university of eastern Africa"
+#d = "I am a christian ,brought up from a catholic denomination"
+#e = "and i am in third year in computer science venturing in cybersecurity"
+#print(b+c+d+e)
+
+
+# a variable represence a value in python 
+# a variable can be reassigned a new value in python
+#a=4
+#b=a
+#print(b)
+#a=6
+#print(b)
+
+
+
+#swapping of variable is a pit complex in python therefore the is need to introduce an extra variable which will represent the first variable, will the first variable then be allowed to repreent the second variable and then  the second variable be assigned the first variable for example
+#vi = 40
+#v2 = 50
+#temp = vi
+#vi = v2
+#v2 = temp
+#print(vi)
+#print(v2)
+
+
+
+
+#if Else Statement example1 in python
+# python is sensitive and whatever statement to be excuted should be intended atleast 4 space in the conditional statement
+#for example
+#a = 20
+#b = 30
+#if a > b:
+#    print("a is greater than b")
+#print("not sure if any of them is greater")
+
+
+
+##### if else statement example2 in python######
+#c = 50
+#d = 50
+#if c > d:
+#    print("c is larger than d")
+#else:
+#    print("d is greater than c")
+#print("  the two values are equal")
+
+
+
+
+
+
+
+####### if else example3 in python######
+#e = 70
+#f = 70
+#if e > f:
+#    print("e is greater than f")
+#elif e == f:
+#    print ("both e and f are equal")
+#else:
+#    print("f is greater than e")
+
+
+
+
+
+
+
+####if else statement example4 in python#######
+# g = 30
+# h = 50
+# if g < h:
+
